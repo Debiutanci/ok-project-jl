510083361a733072e09cbd1e2361df009f3088f6
Jakub <jakub.lemiesiewicz@gmail.com>
2022-11-28 23:40:21 +0100

    oop improve

--- app/app.py
+++ app/app.py
@@ -1,88 +1,92 @@
-mapping_var = {
-    0: "r",
-    1: "s",
-    2: "t",
-    3: "x",
-    4: "y",
-    5: "z",
-}
+MINUS_PATH = -10000000
+
 
 class Graph:
+
+    @staticmethod
+    def generate_keys(quantity):
+        ascii_iter = ord("a")
+        gkeys = []
+        for _ in range(quantity):
+            gkeys.append(chr(ascii_iter))
+            ascii_iter += 1
+        return gkeys
+
     def __init__(self, v) -> None:
         self.v = v
+        self.gkeys = self.generate_keys(v)
         self.stack = []
         self.visited = [False for _ in range(self.v + 1)]
         self.adj = [[] for _ in range(self.v + 1)]
+        self.max_lens = {}
+        self.mapping_var = {}
+        self.__set_max_lens_and_mapping_var()
+        self.__cached_response = None
 
-    def insert_relation(self, vertex_id, ):
-        ...
+    def __set_max_lens_and_mapping_var(self):
+        for gk in self.gkeys:
+            self.max_lens[gk] = []
+        for _ in range(self.v):
+            self.mapping_var[_] = self.gkeys[_]
+
+    def create(self, data):
+        for arr in data:
+            self.adj[arr[0]].append(arr[1])
+
+    def topologicalSortUtil(self, v):
+        self.visited[v] = True
+        for i in self.adj[v]:
+            if (not self.visited[i[0]]):
+                self.topologicalSortUtil(i[0])
+        self.stack.append(v)
 
 
-def topologicalSortUtil(v):
-    global Stack, visited, adj
-    visited[v] = True
-    for i in adj[v]:
-        if (not visited[i[0]]):
-            topologicalSortUtil(i[0])
-    Stack.append(v)
+    def longestPath(self, s):
+        dist = [MINUS_PATH for _ in range(self.v)]
 
+        for i in range(self.v):
+            if self.visited[i] == False:
+                self.topologicalSortUtil(i)
 
-def longestPath(s):
-    max_lens = {
-        "r": [],
-        "s": [],
-        "t": [],
-        "x": [],
-        "y": [],
-        "z": []
-    }
-
-    global Stack, visited, adj, V
-    dist = [-10**9 for i in range(V)]
- 
-    for i in range(V):
-        if (visited[i] == False):
-            topologicalSortUtil(i)
-
-    dist[s] = 0
-    while (len(Stack) > 0):
-
-        u = Stack[-1]
-        del Stack[-1]
-        current_path = [mapping_var[s]]
-        if (dist[u] != -10**9):
-            change = False
-            for i in adj[u]:
+        dist[s] = 0
+        while (len(self.stack) > 0):
+            u = self.stack[-1]
+            del self.stack[-1]
+            current_path = [self.mapping_var[s]]
+            if (dist[u] != MINUS_PATH):
                 change = False
-                if (dist[i[0]] < dist[u] + i[1]):
-                    change = True
-                    dist[i[0]] = dist[u] + i[1]
-                    current_path.append(mapping_var[i[0]])
+                for i in self.adj[u]:
+                    change = False
+                    if (dist[i[0]] < dist[u] + i[1]):
+                        change = True
+                        dist[i[0]] = dist[u] + i[1]
+                        current_path.append(self.mapping_var[i[0]])
 
-                if change:
-                    path_before = [_ for _ in max_lens[mapping_var[u]]]
-                    path_before.extend(mapping_var[u])
-                    max_lens[mapping_var[i[0]]] = path_before
+                    if change:
+                        path_before = [_ for _ in self.max_lens[self.mapping_var[u]]]
+                        path_before.extend(self.mapping_var[u])
+                        self.max_lens[self.mapping_var[i[0]]] = path_before
 
-    response = {
-        "r": {"len": dist[0], "path": max_lens["r"]},
-        "s": {"len": dist[1], "path": max_lens["s"]},
-        "t": {"len": dist[2], "path": max_lens["t"]},
-        "x": {"len": dist[3], "path": max_lens["x"]},
-        "y": {"len": dist[4], "path": max_lens["y"]},
-        "z": {"len": dist[5], "path": max_lens["z"]}
-    }
+        response = {}
+        for gk in self.gkeys:
+            response[gk] = {"len": dist[0], "path": self.max_lens[gk]}
 
-    for k, value in response.items():
-        value["path"].append(k)
+        for k, value in response.items():
+            value["path"].append(k)
 
-    for item in response.items():
-        print(item)
-    
+        self.__cached_response = response
+        return response
+
+    def display_longest_path_info(self):
+        for item in self.__cached_response.items():
+            print(item)
+
+
 if __name__ == '__main__':
-    V, Stack, visited = 6, [], [False for i in range(7)]
-    adj = [[] for i in range(7)]
+    pass
 
+def app():
+    v = 6
     data = [
         [0, [1, 5]],
         [0, [2, 3]],
@@ -96,17 +100,8 @@ if __name__ == '__main__':
         [4, [5, -2]]
     ]
 
-    for arr in data:
-        adj[arr[0]].append(arr[1])
-
+    graph = Graph(v=v)
+    graph.create(data=data)
     s = 0
-    print("Following are longest distances from source vertex ",s)
-    longestPath(s)
-
-# ================================================================================================
-
-
-def app():
-    V = 6
-    stack = ...
-    ...
+    _ = graph.longestPath(s=s)
+    graph.display_longest_path_info()
